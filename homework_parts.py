125e5773e0ae0d2068eec40b91fc0140e0e07471
unknown <mo_h_salim@hotmail.com>
2016-11-04 19:28:05 -0400

    Move over small chunks for part 2 and part 3

--- homework_parts.py
+++ homework_parts.py
@@ -3,8 +3,15 @@ from openpyxl import Workbook
 from sklearn.cluster import KMeans
 from sklearn.random_projection import GaussianRandomProjection
 from sklearn import mixture
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from sklearn.decomposition import FastICA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import time
+import numpy as np
 
-def part1(data, target, x_train, x_test, y_train, y_test):
+def part1(data, target, x_train, y_train, x_test, y_test):
     # K-Means
     # TODO find best n_clusters range (1-50?)
     print('--- KMeans ---')
@@ -37,3 +44,85 @@ def part1(data, target, x_train, x_test, y_train, y_test):
             bench_mark = bench_em(mixture.GaussianMixture(n_components=n, covariance_type=cv_type), 'EM ' + cv_type, n, x_train, y_train, x_test, y_test)
             ws.append(bench_mark)
     wb.save('part-1-em-bench.xlsx')
+
+def part2(data, target, x_train, y_train, x_test, y_test):
+    # PCA
+    print('--- PCA ---')
+    x = []
+    noise_variance_y=[]
+    train_time_y = []
+    for n in range(1, 28):
+        x.append(n)
+        pca = PCA(n_components=n)
+        start = time.time()   
+        pca.fit(x_train)
+        train_time = time.time() - start
+        train_time_y.append(train_time)
+        #print(pca.components_) 
+        #print(pca.explained_variance_) 
+        #print(pca.explained_variance_ratio_) 
+        #print(pca.mean_)
+        #print(pca.n_components_)
+        noise_variance_y.append(pca.noise_variance_)
+
+
+    x_label = 'Number of Components/Clusters'
+    plt.figure(1)
+    plt.xlabel(x_label)
+    plt.ylabel('Noise Variance')
+    plt.plot(x, noise_variance_y)
+
+    plt.figure(2)
+    plt.xlabel(x_label)
+    plt.ylabel('Train Wall Time')
+    plt.plot(x, train_time_y)
+
+    plt.show()
+
+    # ICA
+    print('--- ICA ---')
+    ica = FastICA(n_components=2)
+    ica.fit(x_train)
+    print(ica.components_) 
+    print(ica.mixing_ ) 
+    print(ica.n_iter_)
+
+    # RCA
+    print('--- RCA ---')
+    rca = GaussianRandomProjection(n_components=2)
+    rca.fit(x_train)
+    print(rca.components_) 
+    print(rca.n_components_)
+
+    # LDA
+    print('--- LDA ---')
+    lda = LinearDiscriminantAnalysis(n_components=2)
+    lda.fit(x_train.astype(np.float), y_train.astype(int))
+    print(lda.coef_)
+    print(lda.intercept_)
+    #print(lda.covariance_)
+    print(lda.explained_variance_ratio_)
+    print(lda.means_)
+    print(lda.priors_)
+    print(lda.scalings_)
+    print(lda.xbar_)
+    print(lda.classes_)
+
+def part3(data, target, x_train, y_train, x_test, y_test):
+    # PCA -> KMeans
+    print('--- PCA -> KMeans ---')
+    n = 2
+    pca = PCA(n_components=n).fit(x_train)
+    # In this case the seeding of the centers is deterministic.
+    # Hence we run the k-means algorithm only once with n_init=1.
+    # Else wise it will give a warning and set n_init to 1 anyways.
+    kmeans = KMeans(init=pca.components_, n_clusters=n, n_init=1, random_state=0).fit(x_train)
+    #print(kmeans.labels_)
+    predicted_labels = kmeans.predict(x_test)
+    #print(predicted_labels)
+    # For every array, get the last item. Then squeeze all them together so it's a 1D array instead of a 2D array.
+    expected_labels = y_test
+    #print(expected_labels)
+    score = metrics.accuracy_score(expected_labels, predicted_labels)    
+    print("kmeans with pca " + str(n) + ": " + str(score))
+
--- scikit_sample.py
+++ scikit_sample.py
@@ -1,14 +1,6 @@
 import arff, numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.decomposition import PCA
-from sklearn.decomposition import FastICA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-import matplotlib.pyplot as plt
-from sklearn.random_projection import GaussianRandomProjection
-from sklearn.cluster import KMeans
-import time
-from homework_parts import part1
+from homework_parts import part1, part2, part3
 
 # Get data from arff file.
 dataset = arff.load(open('tic-tac-toe-split.arff', 'r'))
@@ -25,84 +17,6 @@ data = data[:, :feature_count]
 x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
 
 # Run each homework part.
-part1(data, target, x_train, x_test, y_train, y_test)
-#part2(data, target, x_train, x_test, y_train, y_test)
-
-# PCA
-print('--- PCA ---')
-x = []
-noise_variance_y=[]
-train_time_y = []
-for n in range(1, 28):
-    x.append(n)
-    pca = PCA(n_components=n)
-    start = time.time()   
-    pca.fit(x_train)
-    train_time = time.time() - start
-    train_time_y.append(train_time)
-    #print(pca.components_) 
-    #print(pca.explained_variance_) 
-    #print(pca.explained_variance_ratio_) 
-    #print(pca.mean_)
-    #print(pca.n_components_)
-    noise_variance_y.append(pca.noise_variance_)
-
-
-x_label = 'Number of Components/Clusters'
-plt.figure(1)
-plt.xlabel(x_label)
-plt.ylabel('Noise Variance')
-plt.plot(x, noise_variance_y)
-
-plt.figure(2)
-plt.xlabel(x_label)
-plt.ylabel('Train Wall Time')
-plt.plot(x, train_time_y)
-
-plt.show()
-
-# ICA
-print('--- ICA ---')
-ica = FastICA(n_components=2)
-ica.fit(x_train)
-print(ica.components_) 
-print(ica.mixing_ ) 
-print(ica.n_iter_)
-
-# RCA
-print('--- RCA ---')
-rca = GaussianRandomProjection(n_components=2)
-rca.fit(x_train)
-print(rca.components_) 
-print(rca.n_components_)
-
-# LDA
-print('--- LDA ---')
-lda = LinearDiscriminantAnalysis(n_components=2)
-lda.fit(x_train.astype(np.float), y_train.astype(int))
-print(lda.coef_)
-print(lda.intercept_)
-#print(lda.covariance_)
-print(lda.explained_variance_ratio_)
-print(lda.means_)
-print(lda.priors_)
-print(lda.scalings_)
-print(lda.xbar_)
-print(lda.classes_)
-
-# PCA -> KMeans
-print('--- PCA -> KMeans ---')
-n = 2
-pca = PCA(n_components=n).fit(x_train)
-# In this case the seeding of the centers is deterministic.
-# Hence we run the k-means algorithm only once with n_init=1.
-# Else wise it will give a warning and set n_init to 1 anyways.
-kmeans = KMeans(init=pca.components_, n_clusters=n, n_init=1, random_state=0).fit(x_train)
-#print(kmeans.labels_)
-predicted_labels = kmeans.predict(x_test)
-#print(predicted_labels)
-# For every array, get the last item. Then squeeze all them together so it's a 1D array instead of a 2D array.
-expected_labels = y_test
-#print(expected_labels)
-score = metrics.accuracy_score(expected_labels, predicted_labels)    
-print("kmeans with pca " + str(n) + ": " + str(score))
+part1(data, target, x_train, y_train, x_test, y_test)
+part2(data, target, x_train, y_train, x_test, y_test)
+part3(data, target, x_train, y_train, x_test, y_test)
